e09251a25451fd4ff759149f7797c31a91a77caa
devrewoh <137814665+devrewoh@users.noreply.github.com>
2025-07-18 15:46:09 -0600

    Added NerdFont for LazyVim and shortened terminal prompt length

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Word wrap settings
+vim.opt.wrap = true           -- Enable line wrapping
+vim.opt.linebreak = true      -- Break at word boundaries
+vim.opt.breakindent = true    -- Maintain indentation on wrapped lines
+vim.opt.showbreak = "↪ "      -- Show symbol at beginning of wrapped lines
